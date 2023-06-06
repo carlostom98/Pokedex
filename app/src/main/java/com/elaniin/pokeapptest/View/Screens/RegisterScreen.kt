ba9c1b06ec6442ccde37c496025a4b957f6b7068
CARLOS ANDRÉS MARTÍNEZ SUÁREZ <carams500@hotmail.com>
2023-06-05 19:12:01 -0500

    ProyectEnded

--- app/src/main/java/com/elaniin/pokeapptest/View/Screens/RegisterScreen.kt
+++ app/src/main/java/com/elaniin/pokeapptest/View/Screens/RegisterScreen.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -19,11 +20,16 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import coil.ImageLoader
+import coil.compose.rememberAsyncImagePainter
+import coil.decode.ImageDecoderDecoder
 import com.elaniin.pokeapptest.Athentication.SignInState
+import com.elaniin.pokeapptest.R
 import com.elaniin.pokeapptest.Tools.Tools
 import com.elaniin.pokeapptest.View.DestinationScreen
 import com.elaniin.pokeapptest.ViewModel.PokemonViewModel.SignInViewModel
@@ -60,10 +66,10 @@ fun RegisterScreen(navHost: NavHostController) {
         Box(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(20.dp),
-            contentAlignment = Alignment.Center
+                .padding(20.dp)
         ) {
-            Column() {
+            Gifmage(resource = R.drawable.brianshepard_pokeballgo)
+            Column(modifier=Modifier.align(Alignment.BottomCenter)) {
                 ButtonRegister(text = "Google Register") {
                     val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                         .requestEmail()
@@ -73,9 +79,6 @@ fun RegisterScreen(navHost: NavHostController) {
                     launcher.launch(googleSignInClient.signInIntent)
                 }
                 Spacer(modifier = Modifier.height(20.dp))
-                ButtonRegister(text = "Facebook Register") {
-
-                }
             }
         }
     }
@@ -97,11 +100,31 @@ fun ButtonRegister(text: String, onClick: () -> Unit) {
             .height(50.dp),
         colors = ButtonDefaults.buttonColors(Color.Yellow)
     ) {
-        Text(
-            text = text, Modifier.background(Color.Transparent), style = TextStyle(
-                color = Color.Black,
-                fontSize = 20.sp
+        Row() {
+            Image(painter = painterResource(id = R.drawable.registrocongoogle) , contentDescription = "Icono google")
+            Text(
+                text = text, Modifier.background(Color.Transparent), style = TextStyle(
+                    color = Color.Black,
+                    fontSize = 20.sp
+                )
             )
+        }
+    }
+}
+@Composable
+fun Gifmage(resource:Int){
+    val imageLoader = ImageLoader.Builder(LocalContext.current)
+        .components {
+            add(ImageDecoderDecoder.Factory())
+        }.build()
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = rememberAsyncImagePainter(
+                model = resource,
+                imageLoader
+            ), contentDescription = "ImageMainGif", modifier = Modifier
+                .size(400.dp)
+                .align(Alignment.Center)
         )
     }
 }
